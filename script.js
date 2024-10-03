3c9b11b866c478cb8441d0fe17c18cf3275a79fa
GustavoMallorquinRodriguez <159266751+GustavoMallorquinRodriguez@users.noreply.github.com>
2024-10-03 11:55:09 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,7 +9,36 @@ function calcula() {
     y= x*t;
     l= y+x;
     document.getElementById('resposta').innerText="Esse é o valor do primeiro mês " + l;
-    
 
+    let corpo = document.querySelector('#tabela tbody');
+    let linha1 =`
+                <tr>
+                    <td>0</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td>${v.toFixed(2)}</td>
+                </tr>
+                `;
 
+    corpo.innerHTML = linha1;
+
+        for (let par=1; par<=n; par++){
+
+            let linha =`
+                <tr>
+                    <td>${par}</td>
+                    <td>${l.toFixed(2)}</td>
+                    <td>${taxa.toFixed(2)}</td>
+                    <td>${y.toFixed(2)}</td>
+                    <td>${v-y}</td>
+                </tr>
+                `;
+
+            corpo.innerHTML += linha;
+            v = v - x;
+            juros = v * i;
+            x= p - juros;
+
+    }
 }
